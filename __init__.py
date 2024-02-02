5ad850ccbaf18cb6be6395f26d71497bba420ebe
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-02 15:47:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,16 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
-
-def monfr():
-    return "<h2>Bonjour tout le monde !</h2>"
-def hello_world():
-    return render_template('hello.html')
- from flask import jsonify
+from flask import Flask, render_template, jsonify
 from urllib.request import urlopen
 import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def monfr():
+    return "<h2>Bonjour tout le monde !</h2>"
+
+@app.route('/hello/')
+def hello_world():
+    return render_template('hello.html')
 
 @app.route('/paris/')
 def meteo():
@@ -33,6 +29,6 @@ def meteo():
         results.append({'date': dt_value, 'temperature': temp_day_value_celsius})
 
     return jsonify(results=results)
-                                                                                                                                      
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
