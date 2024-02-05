68ebec964589f2ad3d16e45bb5a370ebaaaca052
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-05 21:47:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,5 +34,16 @@ def meteo():
 
     return jsonify(results=results)
 
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
