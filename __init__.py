621881466c850650fdca3c7c4c04b4b4ca70f0fd
babasbelghida <158565982+babasbelghida@users.noreply.github.com>
2024-02-05 21:53:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -45,5 +45,17 @@ def ReadBDD():
     return render_template('read_data.html', data=data)
 
 
+
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
