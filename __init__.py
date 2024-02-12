e8a01fc6736f47052404ba71320b5eb96bce4bdb
Katia BENMOUHOUB <43414553+katiabenmouhoub@users.noreply.github.com>
2024-02-12 17:45:47 +0100

    Update __init__.py
    temperature

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 ## Définition de la route rapport
