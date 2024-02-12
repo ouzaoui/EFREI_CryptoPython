668b6c0e3e43b12da8b8afdb912c4189479c4693
Katia BENMOUHOUB <43414553+katiabenmouhoub@users.noreply.github.com>
2024-02-12 18:07:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,19 +7,11 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 # Définition de la route pour la page de contact
-
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #comm
-  
-if __name__ == "__main__":
-  app.run(debug=True)
-  @app.route("/contact/")
+@app.route("/contact/")
 def contact():
     return render_template("contact.html")
 ## Définition de la route pour la page des prévisions météorologique de Paris pour les 16 prochains jours.
-@app.route('/paris/')
+@app.route("/paris/")
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
@@ -35,13 +27,13 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 # Route pour extraire les minutes d'une information formatée
-@app.route('/extract-minutes/<date_string>')
+@app.route("/extract-minutes/<date_string>")
 def extract_minutes(date_string):
     date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
     minutes = date_object.minute
     return jsonify({'minutes': minutes})
 # Route pour récupérer les commits minute par minute
-@app.route('/commits/')
+@app.route("/commits/")
 def commits():
     url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits' # URL de l'API GitHub pour les commits du repository
     response = requests.get(url)
@@ -57,4 +49,12 @@ def commits():
             commits_per_minute[minute] = 1
     commits_data = [{'minute': minute, 'commits': commits_per_minute[minute]} for minute in sorted(commits_per_minute.keys())] # Création d'une liste de tuples (minute, nombre de commits)
     return jsonify({'commits_data': commits_data}) # Retourner les données au format JSON
+                                                                                                                                       
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #comm
+  
+if __name__ == "__main__":
+  app.run(debug=True)
+  
 
